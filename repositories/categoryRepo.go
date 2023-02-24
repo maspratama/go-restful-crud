c8a3682cc29c25a81a02074c7f7aa25dbe24827b
Singgih Pratama <singgih.vendor.pasti@pegadaian.co.id>
2023-02-24 21:10:48 +0700

    develope models, repositories, helper

---
+++ repositories/categoryRepo.go
@@ -0,0 +1,15 @@
+package repositories
+
+import (
+	"context"
+	"database/sql"
+	"github.com/maspratama/go-restful-crud/models/domain"
+)
+
+type CategoryRepo interface {
+	Save(ctx context.Context, tx *sql.Tx, category domain.Category) domain.Category
+	Update(ctx context.Context, tx *sql.Tx, category domain.Category) domain.Category
+	Delete(ctx context.Context, tx *sql.Tx, category domain.Category)
+	FindById(ctx context.Context, tx *sql.Tx, categoryId int) (domain.Category, error)
+	FindAll(ctx context.Context, tx *sql.Tx) []domain.Category
+}
---
+++ repositories/categoryRepoImpl.go
@@ -0,0 +1,68 @@
+package repositories
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"github.com/maspratama/go-restful-crud/helper"
+	"github.com/maspratama/go-restful-crud/models/domain"
+)
+
+type CategoryRepoImpl struct {
+}
+
+func (repository *CategoryRepoImpl) Save(ctx context.Context, tx *sql.Tx, category domain.Category) domain.Category {
+	SQL := "insert into category(name) values (?)"
+	result, err := tx.ExecContext(ctx, SQL, category.Name)
+	helper.PanicIfError(err)
+
+	id, err := result.LastInsertId()
+	helper.PanicIfError(err)
+
+	category.Id = int(id)
+	return category
+}
+
+func (repository *CategoryRepoImpl) Update(ctx context.Context, tx *sql.Tx, category domain.Category) domain.Category {
+	SQL := "update category set name = ? where id = ?"
+	_, err := tx.ExecContext(ctx, SQL, category.Id)
+	helper.PanicIfError(err)
+
+	return category
+}
+
+func (repository *CategoryRepoImpl) Delete(ctx context.Context, tx *sql.Tx, category domain.Category) {
+	SQL := "delete from category where id = ?"
+	_, err := tx.ExecContext(ctx, SQL, category.Id)
+	helper.PanicIfError(err)
+}
+
+func (repository *CategoryRepoImpl) FindById(ctx context.Context, tx *sql.Tx, categoryId int) (domain.Category, error) {
+	SQL := "select id, name from category where id = ?"
+	rows, err := tx.QueryContext(ctx, SQL, categoryId)
+	helper.PanicIfError(err)
+
+	category := domain.Category{}
+	if rows.Next() {
+		err := rows.Scan(&category.Id, &category.Name)
+		helper.PanicIfError(err)
+		return category, nil
+	} else {
+		return category, errors.New("category is not found")
+	}
+}
+
+func (repository *CategoryRepoImpl) FindAll(ctx context.Context, tx *sql.Tx) []domain.Category {
+	SQL := "select id, name from category"
+	rows, err := tx.QueryContext(ctx, SQL)
+	helper.PanicIfError(err)
+
+	var categories []domain.Category
+	for rows.Next() {
+		category := domain.Category{}
+		err := rows.Scan(&category.Id, category.Name)
+		helper.PanicIfError(err)
+		categories = append(categories, category)
+	}
+	return categories
+}
